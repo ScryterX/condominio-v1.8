6d777607703ae06e5513b1fea019b8b6bcc045d0
ScryterX <josielcardoso@hotmail.com.br>
2022-06-12 17:32:43 -0300

    teste

--- src/main/java/com/controle/condominio/security/SecurityConfiguration.java
+++ src/main/java/com/controle/condominio/security/SecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.controle.condominio.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,12 +20,14 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
-
-public class SecurityConfiguration {
+//@Configuration
+//@EnableWebSecurity
+public class SecurityConfiguration extends WebMvcConfigurerAdapter {
 
     @Autowired
     UserDetailsService userDetailsService;
@@ -41,13 +46,9 @@ public class SecurityConfiguration {
         return (web) -> web.ignoring().antMatchers("/ignore1", "/ignore2");
     }
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    //..."some code"...
+
     }
 }
