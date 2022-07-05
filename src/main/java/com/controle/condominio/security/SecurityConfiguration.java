f306803147fd9c487346bec88ebb4cdde24f08ee
ScryterX <josielcardoso@hotmail.com.br>
2022-07-05 12:33:57 -0300

    Revert "teste"
    This reverts commit 2e82ce0c

--- src/main/java/com/controle/condominio/security/SecurityConfiguration.java
+++ src/main/java/com/controle/condominio/security/SecurityConfiguration.java
@@ -22,14 +22,9 @@ import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.sql.DataSource;
-
-import java.util.Arrays;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
@@ -39,26 +34,25 @@ public class SecurityConfiguration{
     @Autowired
     UserDetailsService userDetailsService;
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated()).httpBasic(withDefaults());
 
-        http
-//                .cors().and().csrf().disable()
+        http.cors().and().csrf().disable()
                 .httpBasic().and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET,"/parking-spot").hasAnyRole("USER", "ADMIN")
-                .antMatchers(HttpMethod.GET,"/parking-spot/*").hasAnyRole("USER", "ADMIN")
-                .antMatchers(HttpMethod.POST,"/parking-spot/*").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/parking-spot/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.POST,"/parking-spot").hasRole("ADMIN")
                 .antMatchers(HttpMethod.PUT,"/parking-spot").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE,"/parking-spot").hasRole("ADMIN")
-                .and().csrf().disable();
+                .antMatchers(HttpMethod.DELETE,"/parking-spot").hasRole("ADMIN");
         return http.build();
     }
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers(HttpMethod.POST, "/parking-spot","/exemplo");
-   }
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring().antMatchers(HttpMethod.GET,"/parking-spot","/exemplo");
+//   }
 
     // create two users, admin and user
     @Autowired
@@ -68,5 +62,7 @@ public class SecurityConfiguration{
                 .withUser("user").password("{noop}password").roles("USER")
                 .and()
                 .withUser("admin").password("{noop}password").roles("ADMIN");
+
+
     }
 }
